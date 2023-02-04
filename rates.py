570528997a92dab3ecd86e69bb5f3eae90a8bcf9
Armin Kask <69580811+arminkask@users.noreply.github.com>
2023-02-04 23:46:45 +0200

    Update rates.py

--- rates.py
+++ rates.py
@@ -43,8 +43,8 @@ vee_temp_max = float(20.2)
 toa_temp_max = float(21.5)
 p_temp_ok = float(18.0)
 k0_temp_ok = float(10.5)
-k1_temp_ok = float(19.0)
-k2_temp_ok = float(19.0)
+k1_temp_ok = float(20.0)
+k2_temp_ok = float(20.0)
 winter_holiday_temp = float(11.0)
 
 logging.basicConfig(
