510f008ed005742a20510a7f30fc2dcc285926c4
Armin Kask <69580811+arminkask@users.noreply.github.com>
2024-02-19 20:03:53 +0200

    Update rates.py

--- rates.py
+++ rates.py
@@ -39,10 +39,10 @@ pool_temp_id = config['SERVER']['POOL_TEMP_ID']
 ##Prices
 kyte_boiler_max_hind = 300.0
 kyte_saast_hind = 200.0
-bassinikytte_hind = 45.0
+bassinikytte_hind = 70.0
 
 ##Temps and humidity
-vee_temp_max = float(10.0)
+vee_temp_max = float(22.0)
 toa_temp_max = float(21.5)
 p_temp_ok = float(18.0)
 k0_temp_ok = float(18.5)
