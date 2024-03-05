24f9d1cf4782900bc8ad2dc1a9e1b55a24cb10b5
Armin Kask <69580811+arminkask@users.noreply.github.com>
2024-03-05 06:19:33 +0200

    Update rates.py

--- rates.py
+++ rates.py
@@ -43,7 +43,7 @@ bassinikytte_hind = 70.0
 
 ##Temps and humidity
 vee_temp_max = float(21.0)
-toa_temp_max = float(21.5)
+toa_temp_max = float(21.0)
 p_temp_ok = float(18.0)
 k0_temp_ok = float(18.5)
 k1_temp_ok = float(20.5)
