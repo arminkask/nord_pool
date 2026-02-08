032eb14a9fc36b8fb636052de778b9a6416643ca
Armin Kask <arminkask@hotmail.com>
2026-02-08 10:22:37 +0200

    1

--- rates.py
+++ rates.py
@@ -201,6 +201,10 @@ def main():
     h2 = cloud.get(k2_dush_id, {}).get("humidity:0", {}).get("rh")
     pool_temp = get_pool_temp(bassein_vee_temp_ip)
 
+    logging.info('=' * 70)
+    logging.info('*' * 70)
+    logging.info('=' * 70)
+
     logging.info(f"PRICE={price} K1={k1} K2={k2} H0={h0} H2={h2} Pool={pool_temp}")
 
     # -------- HUMIDITY --------
